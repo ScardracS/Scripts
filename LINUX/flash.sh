ff29df996b88a7901effc92edac961ea95bbcace
Marco Scardovi <71923153+scardracs@users.noreply.github.com>
2021-03-29 11:59:33 +0200

    Update flash.sh

--- LINUX/flash.sh
+++ LINUX/flash.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-echo "This script is developed & tested by me (ScardracS) for the Motorola Moto X Play/Pure/Style."
-echo "Feel free to use and/or modify it, but please give me proper credits."
-echo "I don't assume any responsibility if You damage your device/s, You have been warned!"
+set -e
+
+if [ "$EUID" -ne 0 ]
+	then echo "Please be sure to run this script as root"
+	exit
+fi
+
+function pause() {
+   read -p "$*"
+}
+
+clear
+echo "This script is developed and tested by me (scardracs)"
+echo "for the Motorola Moto X Play/Pure/Style and distributed under GPL-2 license."
+echo "Feel free to use and/or modify it without limits."
+echo "I don't assume any responsibility if"
+echo "You damage your device/s, You have been warned!"
 echo
+echo "Press any key to continue..."
+pause
+clear
 echo "First of all please select your PC's OS."
 echo "1 + enter for apt (debian/ubuntu/mint)."
 echo "2 + enter for yum (fedora/red-hat)."
@@ -15,17 +32,17 @@ echo "Any answer on that script MUST be lowercase."
 echo
 read A
 case $A in
-		1)	sudo apt install -y adb fastboot
+		1) apt install -y adb fastboot
 		;;
-		2)	sudo yum install android-tools
+		2) yum install android-tools
 		;;
-		3)	sudo pacman -Sy adb fastboot
+		3) pacman -Sy adb fastboot
 		;;
-		4)	sudo emerge --ask android-sdk-update-manager
+		4) emerge --ask android-tools
 		;;
-		5)	sudo apk add android-tools
+		5) apk add android-tools
 		;;
-		*)	echo "Script stopped!" && exit
+		*) echo "Script stopped!" && exit
 	esac
 clear
 echo "REMEMBER, DON'T PRESS ANYTHING DURING THE FLASHING PROCESS!!"
@@ -39,67 +56,67 @@ case $B in
 			y)
 				clear
 				echo "Wipe Script"
-				sudo adb reboot-bootloader
-				sudo fastboot oem fb_mode_set
-				sudo fastboot flash partition gpt.bin
-				sudo fastboot flash bootloader bootloader.img
-				sudo fastboot flash logo logo.bin
-				sudo fastboot flash boot boot.img
-				sudo fastboot flash recovery recovery.img
-				sudo fastboot flash system system.img_sparsechunk.0
-				sudo fastboot flash system system.img_sparsechunk.1
-				sudo fastboot flash system system.img_sparsechunk.2
-				sudo fastboot flash system system.img_sparsechunk.3
-				sudo fastboot flash system system.img_sparsechunk.4
-				sudo fastboot flash system system.img_sparsechunk.5
-				sudo fastboot flash system system.img_sparsechunk.6
-				sudo fastboot flash system system.img_sparsechunk.7
-				sudo fastboot flash system system.img_sparsechunk.8
-				sudo fastboot flash system system.img_sparsechunk.9
-				sudo fastboot flash system system.img_sparsechunk.10
-				sudo fastboot flash modem NON-HLOS.bin
-				sudo fastboot erase modemst1
-				sudo fastboot erase modemst2
-				sudo fastboot flash fsg fsg.mbn
-				sudo fastboot erase cache
-				sudo fastboot erase userdata
-				sudo fastboot erase customize
-				sudo fastboot erase clogo
-				sudo fastboot oem fb_mode_clear
-				sudo fastboot reboot
+				adb reboot-bootloader
+				fastboot oem fb_mode_set
+				fastboot flash partition gpt.bin
+				fastboot flash bootloader bootloader.img
+				fastboot flash logo logo.bin
+				fastboot flash boot boot.img
+				fastboot flash recovery recovery.img
+				fastboot flash system system.img_sparsechunk.0
+				fastboot flash system system.img_sparsechunk.1
+				fastboot flash system system.img_sparsechunk.2
+				fastboot flash system system.img_sparsechunk.3
+				fastboot flash system system.img_sparsechunk.4
+				fastboot flash system system.img_sparsechunk.5
+				fastboot flash system system.img_sparsechunk.6
+				fastboot flash system system.img_sparsechunk.7
+				fastboot flash system system.img_sparsechunk.8
+				fastboot flash system system.img_sparsechunk.9
+				fastboot flash system system.img_sparsechunk.10
+				fastboot flash modem NON-HLOS.bin
+				fastboot erase modemst1
+				fastboot erase modemst2
+				fastboot flash fsg fsg.mbn
+				fastboot erase cache
+				fastboot erase userdata
+				fastboot erase customize
+				fastboot erase clogo
+				fastboot oem fb_mode_clear
+				fastboot reboot
 				clear
 				exit
 			;;
 			n)
 				clear
 				echo "No Wipe Script"
-				sudo adb reboot-bootloader
-				sudo fastboot oem fb_mode_set
-				sudo fastboot flash partition gpt.bin
-				sudo fastboot flash bootloader bootloader.img
-				sudo fastboot flash logo logo.bin
-				sudo fastboot flash boot boot.img
-				sudo fastboot flash recovery recovery.img
-				sudo fastboot flash system system.img_sparsechunk.0
-				sudo fastboot flash system system.img_sparsechunk.1
-				sudo fastboot flash system system.img_sparsechunk.2
-				sudo fastboot flash system system.img_sparsechunk.3
-				sudo fastboot flash system system.img_sparsechunk.4
-				sudo fastboot flash system system.img_sparsechunk.5
-				sudo fastboot flash system system.img_sparsechunk.6
-				sudo fastboot flash system system.img_sparsechunk.7
-				sudo fastboot flash system system.img_sparsechunk.8
-				sudo fastboot flash system system.img_sparsechunk.9
-				sudo fastboot flash system system.img_sparsechunk.10
-				sudo fastboot flash modem NON-HLOS.bin
-				sudo fastboot erase modemst1
-				sudo fastboot erase modemst2
-				sudo fastboot flash fsg fsg.mbn
-				sudo fastboot erase cache
-				sudo fastboot erase customize
-				sudo fastboot erase clogo
-				sudo fastboot oem fb_mode_clear
-				sudo fastboot reboot
+				adb reboot-bootloader
+				fastboot oem fb_mode_set
+				fastboot flash partition gpt.bin
+				fastboot flash bootloader bootloader.img
+				fastboot flash logo logo.bin
+				fastboot flash boot boot.img
+				fastboot flash recovery recovery.img
+				fastboot flash system system.img_sparsechunk.0
+				fastboot flash system system.img_sparsechunk.1
+				fastboot flash system system.img_sparsechunk.2
+				fastboot flash system system.img_sparsechunk.3
+				fastboot flash system system.img_sparsechunk.4
+				fastboot flash system system.img_sparsechunk.5
+				fastboot flash system system.img_sparsechunk.6
+				fastboot flash system system.img_sparsechunk.7
+				fastboot flash system system.img_sparsechunk.8
+				fastboot flash system system.img_sparsechunk.9
+				fastboot flash system system.img_sparsechunk.10
+				fastboot flash modem NON-HLOS.bin
+				fastboot erase modemst1
+				fastboot erase modemst2
+				fastboot flash fsg fsg.mbn
+				fastboot erase cache
+				fastboot erase customize
+				fastboot erase clogo
+				fastboot oem fb_mode_clear
+				fastboot reboot
 				clear
 				exit
 			;;
